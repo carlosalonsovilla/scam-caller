69ccb58aa3c6358b1698789900f1a3cb63605bb5
carlosalonsovilla <cav2589@gmail.com>
2024-10-20 19:33:57 -0400

    Fix typo in script.

--- caller.py
+++ caller.py
@@ -5,23 +5,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#Load environment variables
+
 account_sid = os.getenv('ACCOUNT_SID')
 auth_token = os.getenv('AUTH_TOKEN')
 twilio_number = os.getenv('TWILIO_NUMBER')
 
 client = Client(account_sid, auth_token)
 
+# Initiate call to specified number
+
 def make_call(to_number):
     call = client.calls.create(
         to=to_number,
         from_=twilio_number,
-        url='https://spamcallmessage-6994.twil.io/twiml'
+        url='https://spamcallmessage-6994.twil.io/twiml' #URL hosting Twilio Function
     )
 
-    print(f"Call initiaed with ID: {call.sid}")
+    print(f"Call initiated with ID: {call.sid}")
 
-scammer_number = ''
+scammer_number = '' #Enter the number you would like to call
 
+# This will keep running until the program is stopped (Crtl + c)
 try:
     while True:
         print(f"Calling: {scammer_number}...")
